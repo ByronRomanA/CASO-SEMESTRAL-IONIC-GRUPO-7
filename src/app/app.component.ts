d50519ea9396592043194a11b2ba75ebf92c4d0c
ByronRomanA <by.roman@duocuc.cl>
2022-10-20 18:07:11 -0300

    Subir

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+
+
+  constructor(public alertController: AlertController,
+    public navCtrl: NavController){}
+
+  async salir(){
+    const alert = await this.alertController.create({
+      header: 'Salir',
+      message: '¿Desea cerrar sesion?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+
+          }
+        },{
+          text: 'Si',
+          handler: () => {
+            localStorage.removeItem('ingresado');
+            this.navCtrl.navigateRoot('login');
+          }
+        }
+      ]
+    });
+    await alert.present();
+
+  }
+}
